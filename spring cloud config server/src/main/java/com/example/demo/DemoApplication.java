dbe0a684dd41c0196ab012edb5312de1d3db0550
dajitui <839737769@qq.com>
2018-08-02 10:55:44 +0800

    dajitui

--- spring cloud config server/src/main/java/com/example/demo/DemoApplication.java
+++ spring cloud config server/src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableConfigServer
+@RefreshScope
 public class DemoApplication {
 
 	public static void main(String[] args) {
